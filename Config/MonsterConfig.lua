0a52ac0015af26f4e5e5947dc72037a07744612e
Raysam74 <leraysam74@gmail.com>
2026-01-18 16:48:04 +0100

    Add MonsterConfig.lua with monster attributes

---
+++ Config/MonsterConfig.lua
@@ -0,0 +1,12 @@
+return {
+    baseSpeed = 14,
+    huntSpeed = 22,
+    stunnedSpeed = 6,
+    hearingRange = 120,
+    lightFearRange = 35,
+    ambushCooldown = 10,
+    stunDuration = 4,
+    maxAggroTime = 20
+}
+
+
